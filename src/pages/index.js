b7f2ac9eccbc60f50f2fc7525efc2d6b7f24f7ed
impactrepublic <leo@impactrepublic.com>
2021-02-17 23:34:29 -0500

    creating updating site

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>The good mirrors</title>
+        <title>good mirrors aren't cheap</title>
         <link href="https://fonts.googleapis.com/css2?family=Adamina&family=Orienta&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous"/ >
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" />
